eca98010ac2b80b1a5e8d8e277a7b95b79a510e2
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-11-22 00:42:19 +0100

    Print conversion errors in alert windows

--- demo/api/client.ml
+++ demo/api/client.ml
@@ -3,6 +3,8 @@ module type C = sig
   val list_of_json: string -> (Types.t list, string) result
 end
 
+exception ConversionError of string
+
 module Make (Client : Cohttp_lwt.S.Client) (Conv : C) = struct
   let list ?ctx () =
     let url = "http://dummy.restapiexample.com/api/v1/employees" in
@@ -10,6 +12,6 @@ module Make (Client : Cohttp_lwt.S.Client) (Conv : C) = struct
     (* FIXME handle HTTP errors *)
     let%lwt raw = Cohttp_lwt.Body.to_string body in
     match Conv.list_of_json raw with
-    | Error e -> failwith @@ "list: " ^ e
+    | Error e -> Lwt.fail (ConversionError e)
     | Ok l -> Lwt.return l
 end
--- demo/web/client.ml
+++ demo/web/client.ml
@@ -15,7 +15,7 @@ let _ = Dom_events.(listen Dom_html.document Typ.domContentLoaded) @@ fun _ _ ->
   match Dom_html.getElementById_coerce "l" Dom_html.CoerceTo.ul with
   | None -> failwith "l"
   | Some ul ->
-    Lwt.async (fun () ->
+    Lwt.async (fun () -> Lwt.catch (fun () ->
       let%lwt l = M.list () in
       l |> List.iter (fun e ->
         let li = Dom_html.(createLi document) in
@@ -23,5 +23,11 @@ let _ = Dom_events.(listen Dom_html.document Typ.domContentLoaded) @@ fun _ _ ->
         ignore (ul##appendChild (li :> Dom.node Js.t))
       );
       Lwt.return_unit
-    );
+    ) (function
+      | Api.Client.ConversionError e ->
+        Dom_html.window##alert (Js.string e);
+        Lwt.return_unit
+      | _ ->
+        Lwt.return_unit
+    ));
     false
